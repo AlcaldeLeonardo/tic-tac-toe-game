8e96d3fb4b39105a9d927ab5e0c19aef59ca52fb
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2024-06-14 19:49:07 -0300

    validateCoordenates function is simplified

--- scripts/components/validateCoordenate.js
+++ scripts/components/validateCoordenate.js
@@ -2,7 +2,7 @@ import { gameBoard } from "./gameBoard.js";
 
 export function validateCoordenates(x,y){
     const board = gameBoard.getBoard();
-    if(x < 3 && y < 3 && board[x][y] == null){
+    if(board[x][y] == null){
             return true;
     }else {
         alert("Invalid values");
