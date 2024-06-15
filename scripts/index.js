2d67826f593fffcc86acb8bef0a0deb9b3852208
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2024-06-15 16:49:14 -0300

    litle details fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,53 @@
 import { createUser } from "./components/createUser.js";
 import { gameBoard } from "./components/gameBoard.js";
-import { ModalElements, resultGameElements } from "./components/renderModules/DOMelements.js";
-import { showPlayerMenu, hideResultGame, hidePlayerMenu } from "./components/renderModules/DisplayStatusMenus.js";
+import {
+    ModalElements,
+    resultGameElements,
+} from "./components/renderModules/DOMelements.js";
+import {
+    showPlayerMenu,
+    hideResultGame,
+    hidePlayerMenu,
+} from "./components/renderModules/DisplayStatusMenus.js";
 import { startGame } from "./components/starGame.js";
 
-
-
-const playerOne = createUser ("", "X")
-const playerTwo = createUser ("", "O")
+const playerOne = createUser("", "X");
+const playerTwo = createUser("", "O");
 
 showPlayerMenu();
 
-ModalElements.btnOk.addEventListener("click", ()=> {
-    hidePlayerMenu();
-    startGame(playerOne, playerTwo)
-}
-)
-resultGameElements.playAgain.addEventListener(`click`, ()=> {
+ModalElements.btnOk.addEventListener("click", () => {
+    if (validateNames()) {
+        hidePlayerMenu();
+        startGame(playerOne, playerTwo);
+    }
+});
+resultGameElements.playAgain.addEventListener(`click`, () => {
     gameBoard.resetBoard();
     hideResultGame();
-    startGame(playerOne,playerTwo)
-})
-resultGameElements.newGame2.addEventListener(`click`, ()=> {
+    startGame(playerOne, playerTwo);
+});
+resultGameElements.newGame2.addEventListener(`click`, () => {
     gameBoard.resetBoard();
     showPlayerMenu();
     hideResultGame();
-    resetPlayersScore();
-    startGame(playerOne,playerTwo)
-})
+    if (validateNames()) {
+        resetPlayersScore();
+        startGame(playerOne, playerTwo);
+    }
+});
 
-function resetPlayersScore () {
+function validateNames() {
+    if (
+        ModalElements.inputP1.value != "" &&
+        ModalElements.inputP2.value != ""
+    ) {
+        return true;
+    }
+    return false;
+}
+
+function resetPlayersScore() {
     playerOne.resetScore();
     playerTwo.resetScore();
 }
